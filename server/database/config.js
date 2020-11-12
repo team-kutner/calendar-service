6ac1530537e6bcedc4946f2ab32f22c8cad4ed86
Daniel Lee <dsl8va@gmail.com>
2020-11-11 17:00:55 -0800

    working seed function for listings and dates

--- server/database/config.js
+++ server/database/config.js
@@ -1,5 +1,8 @@
 const mysql = require('mysql2');
 const faker = require('faker');
+const moment = require('moment');
+const seed = require('./seed.js');
+
 const dbName = process.env.DB_SCHEMAS || 'booking';
 
 let db = mysql.createConnection({
@@ -9,31 +12,20 @@ let db = mysql.createConnection({
   database: 'booking'
 });
 
-db.connect(function(err) {
-  if (err) { return console.error('error: ' + err.message); }
-  console.log('Connected to MySQL server');
-});
-
-const desiredListings = 100;
-for (let i = 0; i < desiredListings; i++) {
-  let newListing = [
-    faker.company.catchPhraseDescriptor(),
-    faker.lorem.sentence(),
-    faker.finance.amount(30, 750, 2),
-    faker.finance.amount(10, 120, 2),
-    faker.finance.amount(10, 75, 2),
-    faker.finance.amount(0, 5, 2),
-    faker.random.number({min: 1, max: 500})
-  ];
-  let query = 'INSERT INTO listings (name, description, pricePerNight, cleaningFee, serviceFee, rating, numRatings) VALUES (?, ?, ?, ?, ?, ?, ?)';
-  db.query(query, newListing, (err, result) => {
-    if (err) { console.log(err); } else {
-      console.log('Listing added');
-    }
+let connect = () => {
+  return new Promise((resolve, reject) => {
+    db.connect(function(err) {
+      if (err) { return console.error('error: ' + err.message); }
+      resolve(console.log('Connected to MySQL server'));
+    });
   });
-}
+};
 
-module.exports = db;
+connect()
+  .then(() => { seed.seed(); console.log('here'); })
+  .catch(err => console.log(err));
+
+module.exports.db = db;
 
 // db.query(`CREATE DATABASE IF NOT EXISTS ${dbName};`);
 
--- server/database/seed.js
+++ server/database/seed.js
@@ -1,36 +1,47 @@
 const faker = require('faker');
 const db = require('./config.js');
+const moment = require('moment');
 
-const listings = [];
-const desiredListings = 100;
+let seed = () => {
+  var executed = false;
 
-// for (let i = 0; i < desiredListings; i++) {
-//   let newListing = {
-//     name: faker.lorem.words({min: 2, max: 5}),
-//     description: faker.lorem.sentence(),
-//     pricePerNight: faker.finance.amount(30, 750, 2),
-//     cleaningFee: faker.finance.amount(10, 120, 2),
-//     serviceFee: faker.finance.amount(10, 75, 2),
-//     rating: faker.random.number({min: 0, max: 5, precision: 0.01}),
-//     numRatings: faker.random.number({min: 1, max: 500})
-//   };
-//   listings.push(newListing);
-// }
+  if (!executed) {
+    executed = true;
 
-for (let i = 0; i < desiredListings; i++) {
-  let newListing = [
-    faker.lorem.words({min: 2, max: 5}),
-    faker.lorem.sentence(),
-    faker.finance.amount(30, 750, 2),
-    faker.finance.amount(10, 120, 2),
-    faker.finance.amount(10, 75, 2),
-    faker.random.number({min: 0, max: 5, precision: 0.01}),
-    faker.random.number({min: 1, max: 500})
-  ];
-  let query = 'INSERT INTO listings (name, description, pricePerNight, cleaningFee, serviceFee, rating, numRatings) VALUES (?, ?, ?, ?, ?, ?, ?)';
-  db.query(query, newListing, (err, result) => {
-    if (err) { console.log(err); } else {
-      console.log('Listing added');
+    const desiredListings = 100;
+    for (let i = 0; i < desiredListings; i++) {
+      let newListing = [
+        faker.company.catchPhraseDescriptor(),
+        faker.lorem.sentence(),
+        faker.finance.amount(30, 750, 2),
+        faker.finance.amount(10, 120, 2),
+        faker.finance.amount(10, 75, 2),
+        faker.finance.amount(0, 5, 2),
+        faker.random.number({min: 1, max: 500})
+      ];
+      let query = 'INSERT INTO listings (name, description, pricePerNight, cleaningFee, serviceFee, rating, numRatings) VALUES (?, ?, ?, ?, ?, ?, ?)';
+      db.db.query(query, newListing, (err, result) => {
+        if (err) { console.log(err); } else {
+          console.log('Listing added');
+        }
+      });
     }
-  });
-}
+    var start = moment().format('YYYY-MM-DD');
+    const desiredDays = 356;
+    for (let i = 0; i < desiredDays; i++) {
+      let date = moment(start, 'YYYY-MM-DD').add(i, 'days').format('YYYY-MM-DD');
+      let newDate = [
+        date,
+        faker.random.boolean()
+      ];
+      let query = 'INSERT INTO dates (date, available) VALUES (?, ?)';
+      db.db.query(query, newDate, (err, result) => {
+        if (err) { console.log(err); } else {
+          console.log('Date added');
+        }
+      });
+    }
+  }
+};
+
+module.exports.seed = seed;
