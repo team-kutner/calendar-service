c1bc209a8829005d6bbb30d59f9f70fa56bdcda2
Parker Stafford <parker.stafford92@gmail.com>
2020-12-02 20:01:15 -0800

    Create schema for postgres db

--- postgres_schema.sql
+++ postgres_schema.sql
@@ -1,12 +1,29 @@
-CREATE DATABASE IF NOT EXISTS booking;
-
-USE booking;
 
 CREATE TABLE IF NOT EXISTS listings (
+  id INT GENERATED ALWAYS AS IDENTITY,
   listingID SERIAL PRIMARY KEY,
   name TEXT,
   description TEXT,
   guestMax INTEGER,
   pricePerNigh INTEGER,
-  cleaningFee
-)
+  cleaningFee NUMERIC(5,2),
+  serviceFee NUMERIC(5,2),
+  rating NUMERIC(3,2),
+  numRatings INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS reservations (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  resID SERIAL PRIMARY KEY,
+  listingID INTEGER,
+  checkIn DATE,
+  checkOut DATE,
+  adults INTEGER,
+  children INTEGER,
+  infants INTEGER,
+  totalprice NUMERIC(7,2),
+  CONSTRAINT fk_listing
+    FOREIGN KEY(listingID)
+      REFERENCES listings(listingID)
+        ON DELETE CASCADE
+);
