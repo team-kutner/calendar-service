54b1811a507315648b55a762c7270fe53895bc16
Parker Stafford <parker.stafford92@gmail.com>
2020-12-07 20:07:12 -0800

    Add toFixed function for prices to display change.

--- client/components/reserveInfo.jsx
+++ client/components/reserveInfo.jsx
@@ -4,7 +4,7 @@ const {styled} = window;
 import {ReserveContainer, Fee, Text, TextNote, FeeNight, CostNight, FeeClean, CostClean, FeeService, CostService, FeeTax, CostTax, FeeTotal, CostTotal, Line} from './styles/app.styles.js';
 
 function ReserveInfo ({ price, nights, clean, service }) {
-
+  console.log(price, nights, clean, service)
   let cost = price * nights;
   let taxes = cost * 0.18;
   let total = (cost + clean + service + taxes).toFixed(2);
@@ -15,9 +15,9 @@ function ReserveInfo ({ price, nights, clean, service }) {
       <FeeNight>${price} x {nights} nights</FeeNight>
       <CostNight>${price * nights}</CostNight>
       <FeeClean>Cleaning fee</FeeClean>
-      <CostClean>${clean}</CostClean>
+      <CostClean>${clean.toFixed(2)}</CostClean>
       <FeeService>Service fee</FeeService>
-      <CostService>${service}</CostService>
+      <CostService>${service.toFixed(2)}</CostService>
       <FeeTax>Occupancy taxes and fees</FeeTax>
       <CostTax>${((price*nights)*0.18).toFixed(2)}</CostTax>
       <Line></Line>
