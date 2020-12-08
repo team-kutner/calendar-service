afb7cd25dbd4cb7c21f644de3950e8ba0050e209
Parker Stafford <parker.stafford92@gmail.com>
2020-12-08 14:16:12 -0800

    Set up and add .env file to git ignore. Refactor db connection to use .env file. Add scripts for seeding to package.json

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const app = require('./app');
 const port = 5000;
+const result = require('dotenv').config();
+if(result.error) {
+  console.log(result.error);
+}
+
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
